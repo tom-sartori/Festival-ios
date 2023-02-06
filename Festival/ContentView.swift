874bcdc9965d0d072abb9cc7239d2c75d2466528
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-02-06 20:47:34 +0100

    Sample project.

---
+++ Festival/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Festival
+//
+//  Created by Tom Sartori on 2/6/23.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Festival/FestivalApp.swift
@@ -0,0 +1,18 @@
+//
+//  FestivalApp.swift
+//  Festival
+//
+//  Created by Tom Sartori on 2/6/23.
+//
+//
+
+import SwiftUI
+
+@main
+struct FestivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
