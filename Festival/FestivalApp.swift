e22ff3a57fb04c88dff6c0c0053c2837301f7739
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-03-28 22:57:39 +0200

    Update content view and app.

--- Festival/FestivalApp.swift
+++ Festival/FestivalApp.swift
@@ -11,15 +11,17 @@ import SwiftUI
 @main
 struct FestivalApp: App {
     init() {
-        #if DEBUG
-        var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
-        #if targetEnvironment(macCatalyst)
-        injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
-        #elseif os(iOS)
-        injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
-        #endif
-        Bundle(path: injectionBundlePath)?.load()
-        #endif
+//        #if DEBUG
+//        var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
+//        #if targetEnvironment(macCatalyst)
+//        injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
+//        #elseif os(iOS)
+//        injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
+//        #endif
+//        Bundle(path: injectionBundlePath)?.load()
+//        #endif
+
+        UserDefaults.standard.set(false, forKey: "isAdmin")
     }
 
     var body: some Scene {
