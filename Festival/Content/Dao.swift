b77790235d38b9e539c4a7f7caf9cfc99b2093ea
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-03-29 22:56:39 +0200

    Generic dao.

--- Festival/Content/Dao.swift
+++ Festival/Content/Dao.swift
@@ -12,22 +12,21 @@ class Dao<T: Codable> {
     public let apiUrl = "http://localhost:8080"
 //    public let apiUrl = "https://teewvds053.execute-api.eu-west-3.amazonaws.com"
 
-    @AppStorage("token") var token: String = ""
+    @AppStorage("token") var token: String?
 
     public init() {}
 
-    public func create(url: String, newObject: T) async throws -> Void {
-        let encoded = try! JSONEncoder().encode(newObject)
+    public func create(url: String, object: T) async throws -> Void {
+        let encoded = try! JSONEncoder().encode(object)
 
-        guard let url = URL(string: "api" + url) else {
-            throw HttpError.serverError
+        guard let url = URL(string: "\(apiUrl)\(url)") else {
+            throw HttpError.wrongUrl
         }
 
         var request = URLRequest(url: url)
 
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "POST"
 
         let (_, response) = try! await URLSession.shared.upload(for: request, from: encoded)
@@ -54,8 +53,8 @@ class Dao<T: Codable> {
         }
     }
 
-    public func update(url: String, id: String, updatedObject: T) async throws {
-        let encoded = try! JSONEncoder().encode(updatedObject)
+    public func update(url: String, id: String, object: T) async throws {
+        let encoded = try! JSONEncoder().encode(object)
 
         guard let url = URL(string: "\(apiUrl)\(url)/\(id)") else {
             throw HttpError.wrongUrl
@@ -76,13 +75,13 @@ class Dao<T: Codable> {
     }
 
     public func delete(url: String, id: String) async throws {
-        guard let url = URL(string: "api" + url + "/" + id) else {
-            throw HttpError.serverError
+        guard let url = URL(string: "\(apiUrl)\(url)/\(id)") else {
+            throw HttpError.wrongUrl
         }
 
         var request = URLRequest(url: url)
 
-        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+//        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")    /// TODO
         request.httpMethod = "DELETE"
 
         let (_, response) = try! await URLSession.shared.data(for: request)
