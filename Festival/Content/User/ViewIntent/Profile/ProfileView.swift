4ff13725ea0bb53a4c77baca459e5cffdd769c37
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-03-29 22:58:22 +0200

    User views.

---
+++ Festival/Content/User/ViewIntent/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+// Created by Tom Sartori on 3/22/23.
+//
+
+import SwiftUI
+
+struct ProfileView : View{
+    var body: some View {
+        NavigationView{
+            Form {
+                Button("Se déconnecter", role: .destructive, action: {
+                    UserDAO().logout()
+                })
+            }
+                .navigationTitle(Text("Profil"))
+        }
+    }
+}
